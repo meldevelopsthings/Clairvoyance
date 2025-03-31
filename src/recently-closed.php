666fbbb756b9e3111f30569b71aeae7bd872d3e5
melody <melody@DESKTOP-5E3SRTT>
2025-03-31 16:20:12 +0100

    dumb not working

--- src/recently-closed.php
+++ src/recently-closed.php
@@ -13,7 +13,7 @@ include_once 'validation.php';
 <button id="navbar-toggle" class="navclose:hidden fixed top-0 right-0 p-3 text-3xl text-text-500 bg-darker-500 rounded-[15px] z-50 transition-all duration-700">
         <img src="./img/menu.svg">
     </button>
-<body class="flex">
+<body class="">
 <!-- Sidebar code, allows user to navigate the site -->
         <nav id="navbar" class="text-text-500 text-xl bg-darker-500 list-none m-0 p-0 navclose:w-1/5 w-full h-screen fixed overflow-auto border-r-3 border-border-500 flex flex-col transition-transform duration-700 navclose:translate-x-0 -translate-x-full">
         <div class="flex-grow">
@@ -56,9 +56,15 @@ include_once 'validation.php';
         </a>
         </nav>    
     </div>
-</body>
-<div class="flex-1 p-4 flex flex-col justify-center items-center ml-">
+    <!-- Main Content -->
+    <main class="navclose:pl-[21%] text-center p-4">
         <h1 class="text-text-500 text-5xl">Recently Closed</h1>
-</div>
+        <div class="w-full mt-10 p-4 text-text-500 bg-darker-500 grid grid-cols-3 rounded-full drop-shadow-outer">
+            <p>Name</p>
+            <p>Last Modified</p>
+            <p>Date Created</p>
+        </div>
+    </main>
+</body>
 <script src="navbar.js"></script>
 </html>
